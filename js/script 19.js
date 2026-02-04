6f539a9149789fa8285cf5c94a324b4d0ba0e7a6
VoMaxim <maksim@gmail.com>
2026-02-04 10:20:11 +0300

    первая практика

---
+++ js/script 19.js
@@ -0,0 +1,25 @@
+"use strict";
+
+alert("Здарова кандибоберы Ы)");
+
+const numberOfFilms = +prompt("Сколько раз за сегодня произошел акт дефикации?", '100500');
+const answer1 = prompt("Какую оценку бы Вы поставили цвету гавна");
+const answer11 = prompt("На сколько баллов оценили бы сам процесс?");
+const answer2 = prompt("Вы были одни в этот момент?");
+const answer22 = prompt("Напиздел же, да?)");
+
+// console.log(numberOfFilms);
+// ____________________________________________________________________
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {
+    a: answer1,
+    b: answer11,
+    c: answer2,
+    d: answer22,
+  },
+  actors: {},
+  genres: [],
+  privat: confirm("Прикольная хуйня, да)))?"),
+};
+console.log(personalMovieDB);
