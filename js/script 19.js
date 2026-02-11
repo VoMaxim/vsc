3142e04ca08a232ed8f307be542b578c81f13d17
VoMaxim <maksim@gmail.com>
2026-02-12 00:21:56 +0300

    final 19

--- js/script 19.js
+++ js/script 19.js
@@ -1,25 +1,21 @@
 "use strict";
 
-alert("Здарова кандибоберы Ы)");
+const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", '');
 
-const numberOfFilms = +prompt("Сколько раз за сегодня произошел акт дефикации?", '100500');
-const answer1 = prompt("Какую оценку бы Вы поставили цвету гавна");
-const answer11 = prompt("На сколько баллов оценили бы сам процесс?");
-const answer2 = prompt("Вы были одни в этот момент?");
-const answer22 = prompt("Напиздел же, да?)");
-
-// console.log(numberOfFilms);
-// ____________________________________________________________________
 const personalMovieDB = {
   count: numberOfFilms,
-  movies: {
-    a: answer1,
-    b: answer11,
-    c: answer2,
-    d: answer22,
-  },
+  movies: {},
   actors: {},
   genres: [],
-  privat: confirm("Прикольная хуйня, да)))?"),
+  privat: false
 };
+
+const a = prompt("Один из последних просмотренных фильмов?"),
+  b = prompt("Один из последних просмотренных фильмов?"),
+  c = prompt("На сколько оцените его?"),
+  d = prompt("На сколько оцените его?)");
+
+personalMovieDB.movies[a] = c;
+personalMovieDB.movies[b] = d;
+
 console.log(personalMovieDB);
